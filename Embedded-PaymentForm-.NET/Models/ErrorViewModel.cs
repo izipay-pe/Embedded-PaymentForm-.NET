b359bbd3f23c1364437df0f157f7cd31de9603cb
Jeanpier Araujo <soporteecommerce@izipay.pe>
2024-10-31 13:36:03 -0500

    Formulario Incrustado Actualizado

---
+++ Embedded-PaymentForm-.NET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Embedded_PaymentForm_.NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
